3ef46d59305da535192943c92f1e5064d1c09ea6
Kyungmin SOng <skm0626@kookmin.ac.kr>
2018-06-18 23:13:00 +0900

    My second commit

--- final-proj/picknumber.py
+++ final-proj/picknumber.py
@@ -8,35 +8,32 @@ class Number:
         self.trials = 0
 
     def newGame(self, amount):
-        self.secret = random.randint(1, amount+1)
+        self.secret = random.randint(1, amount)
 	self.digit = amount
 	self.trials = 0
 
     def guess(self, userGuess):
         self.trials += 1
         if 0 < userGuess < self.secret:
-            print "You have to type bigger number."
+            return "You have to type bigger number."
         elif userGuess > self.secret:
-            print "You have to type smaller number."
+            return "You have to type smaller number."
         elif userGuess == 0:
-            print"0 is out of range"
+            return"0 is out of range"
         elif userGuess == self.secret:
-            print "FINISH"
+            return "FINISH"
 
     def getGuessCount(self):
         return self.trials
-##TEST
+
 if __name__== '__main__':
     s = Number()
     count = int(input("Set Range : "))
     s.newGame(count)
     x = 0
-    while x != s.secret:
+    while x != "FINISH":
         inputStr = int(input("Your Guess : "))
-        while inputStr > count:
-            print("input %d digits!" %count)
-            inputStr = int(input("Your Guess : "))
-        x = s.guess(inputStr)
-        print( )
+	x=s.guess(inputStr)
+	print(x)        
     guessCount = s.getGuessCount()
     print("SUCCESS in %d trials" %guessCount)
